eb9f0d28107637a3035d6f5037ae8caee82cb908
Maxim Makarov <makarovmaxqa@gmail.com>
2023-10-31 19:02:24 +0100

    Initial Commit

---
+++ TwoFigures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoFigures
+//
+//  Created by Maxim Makarov on 31.10.2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
